e493a076562aa0cc6bf8307ec625d80197fe2958
jasmin <jasminchakma895@gmail.com>
2024-01-31 10:07:17 +0600

    reviews swiper updated

--- src/Components/Reviews/Reviews.jsx
+++ src/Components/Reviews/Reviews.jsx
@@ -1,15 +1,74 @@
 import ReviewSwiper from "../Swipers/ReviewSwiper";
 const Reviews = () => {
+  const reviews = [
+    {
+      id: 1,
+      clientName: "Afzal Khattak",
+      clientImg:
+        "https://lh3.googleusercontent.com/a-/ALV-UjVNR5THHVht9Fpg6Hf4wopCT9qYoz9OMxObk7NUFxSyzg=w60-h60-p-rp-mo-ba3-br100",
+      review: ` The communication with the company was seamless. They were responsive to my inquiries, and scheduling the service was hassle-free. The pricing was transparent, and there were no hidden fees or surprises.`,
+    },
+    {
+      id: 2,
+      clientName: "Muhammad Shoaib",
+      clientImg:
+        "https://lh3.googleusercontent.com/a-/ALV-UjXzariwZMf6rKl4uyJwv9YPGPCnoGgks3k4gwAP3ggkegY=w60-h60-p-rp-mo-br100",
+      review: `the team was friendly and approachable, making the entire process a pleasant experience. They were receptive to my specific requests and went above and beyond to ensure I was satisfied with the results.`,
+    },
+    { id: 3,
+      clientName: "Peter Hanna",
+      clientImg:
+        "https://lh3.googleusercontent.com/a/ACg8ocJOrlIlBwZUHVpkv5dOv1memp8WoETCGR22bfaMl9Nv=w60-h60-p-rp-mo-br100",
+      review: `Outstanding end-of-lease cleaning! Punctual, meticulous, and friendly team. Impressed by their attention to detail. Highly recommend for a stress-free move-out experience! ⭐️⭐️⭐️⭐️⭐️`,
+    },
+    { 
+      id: 4,
+      clientName: "Sarah khattak",
+      clientImg:
+        "https://lh3.googleusercontent.com/a/ACg8ocJEHolaJzCWxu5KEWcftYoVCOPIzJ4J2ifNrn5IFziM=w60-h60-p-rp-mo-br100",
+      review: `I recently hired Oz Shine Cleaners for a deep cleaning service, and I am absolutely thrilled with the results. From the initial contact to the completion of the job, the entire experience was seamless and professional.`,
+    },
+    {
+      id: 5,
+      clientName: "Maryam Fayyaz",
+      clientImg:
+        "https://lh3.googleusercontent.com/a/ACg8ocKL5jCbTsb1n8P0FA5qG-iYZqXUw3Ex53vUo5mtnNqZ=w60-h60-p-rp-mo-br100",
+      review: `I recently had the pleasure of using their services, and I must say, I am thoroughly impressed. From start to finish, the experience was top-notch.`,
+    },
+    {
+      id: 6,
+      clientName: "Dawood Haider",
+      clientImg:
+        "https://lh3.googleusercontent.com/a-/ALV-UjVTKD4SpXnzFFtwT70lbKeS2SSuBTPsLicYQ61kaV_eSQ=w60-h60-p-rp-mo-br100",
+      review: `I highly recommend Oz Shine Cleaners to anyone in need of a reliable and thorough cleaning service. They are now my go-to cleaning company, and I look forward to scheduling regular cleanings with them in the future. Thank you, Oz Shine Cleaners, for making my home shine!.`,
+    },
+    {
+      id: 7,
+      clientName: "Ahmad Jutt",
+      clientImg:
+        "https://lh3.googleusercontent.com/a/ACg8ocIGjNcekGXnq_VHXau26Hu8G2kzE4vT6Jnd3Qz7w51w=w60-h60-p-rp-mo-br100",
+      review: `I recently used Oz Shine Cleaners for their services, and I'm extremely satisfied with the results. The team was punctual, professional, and thorough in their cleaning. My space now looks impeccable, and they exceeded my expectations. I highly recommend Oz Shine Cleaners for their top-notch service and attention to detail.`,
+    },
+    {
+      id: 8,
+      clientName: "Fayyaz ur Rehman",
+      clientImg:
+        "https://lh3.googleusercontent.com/a/ACg8ocKma5p2Uyvnt_WEbmrp1OdHU59_W8DEYALcfDNgm2Dp=w60-h60-p-rp-mo-br100",
+      review: `Communication with the team was seamless. They were responsive to my inquiries, provided a transparent quote, and exceeded my expectations in terms of efficiency and thoroughness. It's evident that customer satisfaction is a priority for them.`,
+    },
+  ];
+
   return (
     <div className="py-20 px-5 lg:px-0">
       <p className="text-center font-bold text-4xl lg:text-5xl xsm:text-2xl">
         What Our Client {"Say’s"}
       </p>
       <p className="text-center text-sm lg:text-lg mt-4 text-gray-500 mb-20 ">
-        This is what clients have been saying after using the great service we
-        do for clients
+        {/* This is what clients have been saying after using the great service we
+        do for clients */}
+        This is what our customers have been expressing through Google reviews after experiencing the exceptional service we provide.
       </p>
-      <ReviewSwiper/>
+      <ReviewSwiper reviews={reviews}></ReviewSwiper>
     </div>
   );
 };
--- src/Components/Swipers/ReviewSwiper.jsx
+++ src/Components/Swipers/ReviewSwiper.jsx
@@ -4,56 +4,22 @@ import "swiper/css/autoplay";
 import "swiper/swiper-bundle.css";
 import { Autoplay, Pagination, Navigation, FreeMode } from "swiper/modules";
 import { TiStarFullOutline } from "react-icons/ti";
+import PropTypes from "prop-types";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-const ReviewSwiper = () => {
-  const reviews = [
-    {
-      clientName: "Afzal Khattak",
-      clientImg:
-        "https://lh3.googleusercontent.com/a-/ALV-UjVNR5THHVht9Fpg6Hf4wopCT9qYoz9OMxObk7NUFxSyzg=w60-h60-p-rp-mo-ba3-br100",
-      review: ` The communication with the company was seamless. They were responsive to my inquiries, and scheduling the service was hassle-free. The pricing was transparent, and there were no hidden fees or surprises.`,
-    },
-    {
-      clientName: "Muhammad Shoaib",
-      clientImg:
-        "https://lh3.googleusercontent.com/a-/ALV-UjXzariwZMf6rKl4uyJwv9YPGPCnoGgks3k4gwAP3ggkegY=w60-h60-p-rp-mo-br100",
-      review: `the team was friendly and approachable, making the entire process a pleasant experience. They were receptive to my specific requests and went above and beyond to ensure I was satisfied with the results.`,
-    },
-    {
-      clientName: "Peter Hanna",
-      clientImg:
-        "https://lh3.googleusercontent.com/a/ACg8ocJOrlIlBwZUHVpkv5dOv1memp8WoETCGR22bfaMl9Nv=w60-h60-p-rp-mo-br100",
-      review: `Outstanding end-of-lease cleaning! Punctual, meticulous, and friendly team. Impressed by their attention to detail. Highly recommend for a stress-free move-out experience! ⭐️⭐️⭐️⭐️⭐️`,
-    },
-    {
-      clientName: "Sarah khattak",
-      clientImg:
-        "https://lh3.googleusercontent.com/a/ACg8ocJEHolaJzCWxu5KEWcftYoVCOPIzJ4J2ifNrn5IFziM=w60-h60-p-rp-mo-br100",
-      review: `I recently hired Oz Shine Cleaners for a deep cleaning service, and I am absolutely thrilled with the results. From the initial contact to the completion of the job, the entire experience was seamless and professional.`,
-    },
-    {
-      clientName: "Maryam Fayyaz",
-      clientImg:
-        "https://lh3.googleusercontent.com/a/ACg8ocKL5jCbTsb1n8P0FA5qG-iYZqXUw3Ex53vUo5mtnNqZ=w60-h60-p-rp-mo-br100",
-      review: `I recently had the pleasure of using their services, and I must say, I am thoroughly impressed. From start to finish, the experience was top-notch.`,
-    },
-    {
-      clientName: "Dawood Haider",
-      clientImg:
-        "https://lh3.googleusercontent.com/a-/ALV-UjVTKD4SpXnzFFtwT70lbKeS2SSuBTPsLicYQ61kaV_eSQ=w60-h60-p-rp-mo-br100",
-      review: `I highly recommend Oz Shine Cleaners to anyone in need of a reliable and thorough cleaning service. They are now my go-to cleaning company, and I look forward to scheduling regular cleanings with them in the future. Thank you, Oz Shine Cleaners, for making my home shine!.`,
-    },
-    {
-      clientName: "Ahmad Jutt",
-      clientImg: 'https://lh3.googleusercontent.com/a/ACg8ocIGjNcekGXnq_VHXau26Hu8G2kzE4vT6Jnd3Qz7w51w=w60-h60-p-rp-mo-br100',
-      review: `I recently used Oz Shine Cleaners for their services, and I'm extremely satisfied with the results. The team was punctual, professional, and thorough in their cleaning. My space now looks impeccable, and they exceeded my expectations. I highly recommend Oz Shine Cleaners for their top-notch service and attention to detail.`,
-    },
-    {
-      clientName: "Fayyaz ur Rehman",
-      clientImg: 'https://lh3.googleusercontent.com/a/ACg8ocKma5p2Uyvnt_WEbmrp1OdHU59_W8DEYALcfDNgm2Dp=w60-h60-p-rp-mo-br100',
-      review: `Communication with the team was seamless. They were responsive to my inquiries, provided a transparent quote, and exceeded my expectations in terms of efficiency and thoroughness. It's evident that customer satisfaction is a priority for them.`,
-    },
-  ];
+const ReviewSwiper = ({ reviews }) => {
+  const [showFullContentMap, setShowFullContentMap] = useState({});
+
+  const handleToggleContent = (reviewId) => {
+    setShowFullContentMap((prevShowFullContentMap) => {
+      return {
+        ...prevShowFullContentMap,
+        [reviewId]: !prevShowFullContentMap[reviewId],
+      };
+    });
+  };
+
   return (
     <div>
       <Swiper
@@ -91,46 +57,89 @@ const ReviewSwiper = () => {
         freeMode={true}
         modules={[FreeMode, Pagination, Navigation, Autoplay]}
         className="mySwiper"
-        allowTouchMove={false}
+        allowTouchMove={true}
         autoplay={{
-          delay: 2000,
-          disableOnInteraction: false,
+          delay: 3000,
+          disableOnInteraction: true,
+          pauseOnMouseEnter: true,
         }}
       >
         <div>
-          {reviews.map((review) => (
-            <SwiperSlide key={review.clientName}>
-              <div className="space-y-10 border-2 p-10 rounded-lg">
-                <img
-                  src="https://i.ibb.co/S7bmTTJ/307abac1c2751b920c46a4b4fd0910d8.png"
-                  alt=""
-                  className="w-10 bg-primary-c rounded-full"
-                />
-                <p className="text-gray-500">
-                  {review.review.length === 118
-                    ? review.review
-                    : review.review.slice(0, 119) + '...'}
-                </p>
-                <div className="flex justify-between">
-                  <div className="flex gap-3 items-center">
-                    <img className="rounded-full w-10" src={review.clientImg} alt="" />
-                    <p>{review.clientName}</p>
+          {reviews.map((review) => {
+            const truncatedReview =
+              review.review.length === 118
+                ? review.review
+                : review.review.slice(0, 119);
+
+            const showFullContent = showFullContentMap[review.id];
+
+            return (
+              <SwiperSlide key={review.clientName}>
+                <div className="space-y-10 border-2 p-10 rounded-lg">
+                  <div>
+                    <img
+                      src="https://i.ibb.co/S7bmTTJ/307abac1c2751b920c46a4b4fd0910d8.png"
+                      alt=""
+                      className="w-10 bg-primary-c rounded-full"
+                    />
                   </div>
-                  <div className="flex items-center">
-                    <TiStarFullOutline className="text-amber-500"></TiStarFullOutline>
-                    <TiStarFullOutline className="text-amber-500"></TiStarFullOutline>
-                    <TiStarFullOutline className="text-amber-500"></TiStarFullOutline>
-                    <TiStarFullOutline className="text-amber-500"></TiStarFullOutline>
-                    <TiStarFullOutline className="text-amber-500"></TiStarFullOutline>
+                  <div>
+                    <p className="text-gray-500">
+                      {showFullContent
+                        ? review.review
+                        : truncatedReview + "..."}
+                    </p>
+                    {review.review.length > 118 && (
+                      <button
+                        onClick={() => handleToggleContent(review.id)}
+                        className="font-bold"
+                      >
+                        {showFullContent ? "Show less" : "Show more"}
+                      </button>
+                    )}
+                  </div>
+                  <div className="flex justify-between">
+                    <div className="flex gap-3 items-center">
+                      <img
+                        className="rounded-full w-10"
+                        src={review.clientImg}
+                        alt=""
+                      />
+                      <p>{review.clientName}</p>
+                    </div>
+                    <div className="space-y-2">
+                      <div className="flex items-center justify-center">
+                        <Link
+                          to={`https://www.google.com/search?q=oz%20shine%20cleaners&rlz=1C1ONGR_enBD1089BD1089&oq=oz%20shi&gs_lcrp=EgZjaHJvbWUqCAgAEEUYJxg7MggIABBFGCcYOzIMCAEQABhDGIAEGIoFMgYIAhBFGDkyCAgDEEUYJxg7MgcIBBAAGIAEMgcIBRAAGIAEMgYIBhBFGDwyBggHEEUYPNIBCDEzMjRqMGoxqAIAsAIA&sourceid=chrome&ie=UTF-8&fbclid=IwAR1X2na6Y7FPwO0dGxYDSXdbp0ic6xndKKxw191JH6vIWjUfwGo_eu8qvg8#lrd=0x6ad64500046398b1:0x270b937`}
+                        >
+                          <img
+                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/1024px-Google_%22G%22_logo.svg.png"
+                            alt=""
+                            className="w-5 h-5"
+                          />
+                        </Link>
+                      </div>
+                      <div className="flex items-center">
+                        <TiStarFullOutline className="text-amber-500"></TiStarFullOutline>
+                        <TiStarFullOutline className="text-amber-500"></TiStarFullOutline>
+                        <TiStarFullOutline className="text-amber-500"></TiStarFullOutline>
+                        <TiStarFullOutline className="text-amber-500"></TiStarFullOutline>
+                        <TiStarFullOutline className="text-amber-500"></TiStarFullOutline>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </SwiperSlide>
-          ))}
+              </SwiperSlide>
+            );
+          })}
         </div>
       </Swiper>
     </div>
   );
+};
+
+ReviewSwiper.propTypes = {
+  reviews: PropTypes.array.isRequired,
 };
 
 export default ReviewSwiper;
